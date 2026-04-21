fe4e780d7352d48a7cdb2445ae43c50f17e0d1fb
s0npaRi11 <paragmahale36@gmail.com>
2026-04-21 10:37:46 +0530

    added port config

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', require('./crud.js'))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started!!!')
 })
